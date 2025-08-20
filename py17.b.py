c8b38580ac5faa952996096a743f0e1172e6b81d
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-20 16:03:29 +0530

    py17.b

---
+++ py17.b.py
@@ -0,0 +1,11 @@
+print("Sum of N numbers")
+print("----------------")
+m1=int(input("Enter the strating number:"))
+m2=int(input("Enter the ending number:"))
+print("Result")
+sum=0
+for i in range(m1,m2+1):
+  print("+",+i)
+  sum=m1+i
+print("sum")
+print(sum)                   
