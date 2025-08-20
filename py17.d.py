4a8a0f70e88f3144ac7124d30f27394ead9c698b
rahathunnishaabdul <rahathunnisha24@gmail.com>
2025-08-20 16:04:40 +0530

    py17.d

---
+++ py17.d.py
@@ -0,0 +1,16 @@
+print("Sum of N numbers")
+print("---------------")
+SN=int(input("Enter the strating number:"))
+EN=int(input("Enter the ending number:"))
+print("Result")
+print("--------")
+print("series")
+count=0
+sum=0
+for i in range(SN,EN+1):
+    if(i%2==)and(1%3==0):
+print("+", +i)
+    sum=sum+i
+    count+=1
+print("sum value:",sum)
+print("Count value:",count)
